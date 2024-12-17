11c21456fc4e6637b31c910384a76bf8dc8475be
Harshitha1720 <harshikg99@gmail.com>
2024-12-17 21:28:40 +0530

    Add initial WeatherExtractor class

---
+++ src/weather_extractor.py
@@ -0,0 +1,8 @@
+class WeatherExtractor:
+    def __init__(self):
+        self.base_url = "http://api.openweathermap.org/data/2.5/weather"
+        
+    def get_weather_data(self):
+        print("Weather data extraction started")
+        # We'll add the actual implementation later
+        return None
